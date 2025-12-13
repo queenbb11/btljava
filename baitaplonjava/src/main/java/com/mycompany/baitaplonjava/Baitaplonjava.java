7b99db455cef798206b56ad1e36a5df61a195e1c
hermitdna <duonganh43507@gmail.com>
2025-12-14 02:00:31 +0700

    hehe

--- baitaplonjava/src/main/java/com/mycompany/baitaplonjava/Baitaplonjava.java
+++ baitaplonjava/src/main/java/com/mycompany/baitaplonjava/Baitaplonjava.java
@@ -12,6 +12,7 @@ public class Baitaplonjava {
 
     public static void main(String[] args) {
         System.out.println("Hellofghjk");
-
+        System.out.println("hell hehee");
+       
     }
 }
